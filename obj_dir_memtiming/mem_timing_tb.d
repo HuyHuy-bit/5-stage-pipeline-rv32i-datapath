04f1fe23f128f4d9924f36501aabc5182297ca81
HuyHuy-bit <huynguyenngoccbg@gmail.com>
2026-07-28 00:37:06 +0700

    Stop tracking the mem_timing build directory

--- obj_dir_memtiming/mem_timing_tb.d
+++
@@ -1,6 +0,0 @@
-mem_timing_tb.o: ../tb/mem_timing_tb.cpp Vmem_timing.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h
